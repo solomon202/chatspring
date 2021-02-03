76c4bccbed14d9696981281fee2dc87b16170230
solomon202 <solomon202.pi@yandex.ru>
2021-02-04 01:22:00 +0300

    first commit

---
+++ src/main/java/com/example/demo/repos/UserRepo.java
@@ -0,0 +1,10 @@
+package com.example.demo.repos;
+
+import com.example.demo.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+
+    User findByActivationCode(String code);
+}
